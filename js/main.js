d4d77e3c0529286d5618f5a5bef76aa2581eb4a4
christopherpowell <chrispylcfc@gmail.com>
2017-11-08 16:30:59 +0000

    Rough Outline Content Added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-// This will include the main JS fucntionality.
+/*
+  This will include the main JS fucntionality.
+*/
+
+(function(){
+  let navButton = document.getElementById("menu");
+  let navMenu = document.getElementById("nav-menu");
+
+  let toggle = false; // hidden at first
+  function toggleMenu(){
+    if (toggle) { // true: it's visible
+      navMenu.classList.remove("show-menu"), // hide it
+      toggle = false
+    }
+    else { // false: it's hidden
+      navMenu.classList.add("show-menu"), // show it
+      toggle = true
+    }
+  }
+
+  navButton.addEventListener("click", toggleMenu);
+
+})();
