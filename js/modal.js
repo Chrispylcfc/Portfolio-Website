119ed8f686be1f387732c68092554c7226f93163
christopherpowell <chrispylcfc@gmail.com>
2017-12-01 17:59:30 +0000

    Modal Functionality Working & Initial Styling

--- js/modal.js
+++ js/modal.js
@@ -5,12 +5,17 @@
 (function(){
   let modal = document.getElementById('contact-modal');
   let button = document.getElementById("contact-button");
+  let close = document.getElementById("close-icon");
 
   function displayModal() {
-    //modal.style.display = "block";
-    // modal.classList.add("");
+    modal.classList.add("show-modal");
+  }
+
+  function hideModal() {
+    modal.classList.remove("show-modal");
   }
 
   button.addEventListener("click", displayModal);
+  close.addEventListener("click", hideModal);
 
 })();
