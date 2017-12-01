07a733d51480d6dea6e6f060d59a9d387c17e554
christopherpowell <chrispylcfc@gmail.com>
2017-12-01 17:15:06 +0000

    Added modal.js file

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+/*
+  JS for Modal Functionality
+*/
+
+(function(){
+  let modal = document.getElementById('contact-modal');
+  let button = document.getElementById("contact-button");
+
+  function displayModal() {
+    //modal.style.display = "block";
+    // modal.classList.add("");
+  }
+
+  button.addEventListener("click", displayModal);
+
+})();
